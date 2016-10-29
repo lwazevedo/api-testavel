db07ac474d924b27ffce41be775e421d81e3c038
Luiz Azevedo <lwnazevedo@gmail.com>
2016-10-29 16:45:30 -0200

    video aula 8 testes unitarios

--- controllers/books.js
+++ controllers/books.js
@@ -1,9 +1,24 @@
+const defaultResponse = (data, statusCode = 200) => ({
+    data,
+    statusCode
+});
+
+const errorResponse = (message, statusCode = 400) => defaultResponse({
+    error: message,
+}, statusCode);
+
+
 class BooksController {
     constructor(Books) {
         this.Books = Books;
     }
 
     getAll() {
+        return this.Books.findAll({})
+            .then(result => defaultResponse(result))
+            .catch(err => errorResponse(error.message));
 
     }
 }
+
+export default BooksController;
