083ea1c67e48b67fc87d878e482cde11522d3dbe
Luiz Azevedo <lwnazevedo@gmail.com>
2016-11-04 01:13:23 -0200

    Video aula 15 Criacao do jwt e passport

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import config from './config/config';
 import datasource from './config/datasource';
 import booksRouter from './routes/books';
 import usersRouter from './routes/users';
+import authRouter from './routes/auth';
+import autorization from './auth';
 
 const app = express();
 app.config = config;
@@ -12,8 +14,13 @@ app.datasource = datasource(app);
 app.set('port', 7000);
 app.use(bodyParser.json());
 
+const auth = autorization(app);
+app.use(auth.initialize());
+app.auth = auth;
+
 booksRouter(app);
 usersRouter(app);
+authRouter(app);
 
 
 // Igual exports.module
